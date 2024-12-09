afcbedb4e4fad9527aa26149117e0debe0d288da
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-09 20:56:17 +0700

    Update conflix-ddos.py

--- conflix-ddos.py
+++ conflix-ddos.py
@@ -37,11 +37,11 @@ async def increment_view_count(session):
     try:
         async with session.get(url) as response:
             if response.status == 200:
-                print("[+]  \033[32mRequest Sent  \033[33m" +url+"  \033[34mattack..!! \033[92m%s\033[0m")
-                print("[+]  \033[95mRequest Sent  \033[96m" +url+"  \033[92mattack..!! \033[92m%s\033[0m")
+                print("[+]  \033[32mRequest Sent  \033[33m" +url+"  \033[34mattack..!! \033[92m" +str(u)+ "\033[0m")
+                print("[+]  \033[95mRequest Sent  \033[96m" +url+"  \033[92mattack..!! \033[92m" +str(u)+ "\033[0m")
             else:
-                print("[!]  \033[96mRequest Sent  \033[32m" +url+"  \033[33mattack..!! \033[92m%s\033[0m")
-                print("[!]  \033[94mRequest Sent  \033[96m" +url+"  \033[37mattack..!! \033[92m%s\033[0m")
+                print("[!]  \033[96mRequest Sent  \033[32m" +url+"  \033[33mattack..!! \033[92m" +str(u)+ "\033[0m")
+                print("[!]  \033[94mRequest Sent  \033[96m" +url+"  \033[37mattack..!! \033[92m" +str(u)+ "\033[0m")
     except aiohttp.ClientError as e:
         print("[💥]  \033[34mAn error occurred:\033[0m", e)
 
